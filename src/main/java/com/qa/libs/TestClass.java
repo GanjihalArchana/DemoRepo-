81dc437f0201bb9b0baf983ccb887a0921aa1c7b
LAPTOP-CIB4V5DS\Archana <archanaganjihal@gmail.com>
2018-03-02 15:45:27 -0800

    First git commmit for my selenium code

---
+++ src/main/java/com/qa/libs/TestClass.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class TestClass {
+
+	public void Test(){
+	System.out.println("Hello , its just a demo code");
+
+	}
+
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestUtil {
+
+	public void loginPage() {
+		System.out.println("Its just a login Page");
+	}
+
+}
