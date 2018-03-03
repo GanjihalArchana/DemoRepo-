1d7d49e3c3d8d12e7bd2495e62956988e23aaafb
LAPTOP-CIB4V5DS\Archana <archanaganjihal@gmail.com>
2018-03-02 16:29:32 -0800

    added searchpage feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,10 @@
 package com.qa.pages;
 
 public class LoginPage {
-
+	public void loginPage() {
+		System.out.println("Its just a login Page");
+	}
+public void loginsetmethod(){
+	System.out.println("login set method test");
+}
 }
