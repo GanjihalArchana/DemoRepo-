8a6f7cc54e5f65cc58c21a1fdfe582aa2cc1b64e
DELL5465\Swaroop.Lingsugur <mahesh.lingsugur@ca.abb.com>
2018-04-18 07:59:29 -0700

    Changes from PC2

--- src/main/java/com/qa/libs/TestClass.java
+++ src/main/java/com/qa/libs/TestClass.java
@@ -3,9 +3,9 @@ package com.qa.libs;
 public class TestClass {
 
 	public void Test(){
-	System.out.println("Hello , its just a demo code");
+	System.out.println("Hello , it's just a demo code");
 	System.out.println("Changes made");
-
+	System.out.println("Changes from PC2");
 	}
 
 }
